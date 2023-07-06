4bc911282112abf539c5868a97defa9df87173b7
tushar.dutt <tushar.dutt@newvision-software.com>
2023-07-06 18:10:26 +0530

    Final Update

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-progress-bar',
@@ -8,9 +8,8 @@ import { Router } from '@angular/router';
 export class ProgressBarComponent {
   url!: string;
   bgClass = 'default';
-  element: HTMLElement | undefined;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,private element: ElementRef,private renderer: Renderer2) {
     // router.events.subscribe((val)=>console.log(val));
     this.getHeaderStyle();
   }
@@ -22,6 +21,10 @@ export class ProgressBarComponent {
     let targetDiv2 = document.getElementById("numberCircle2");
     let targetDiv3 = document.getElementById("numberCircle3");
     let targetDiv4 = document.getElementById("numberCircle4");
+    let l1 = document.getElementById("line1");
+    let l2 = document.getElementById("line2");
+    let l3 = document.getElementById("line3");
+    console.log(l1);
     this.router.events.subscribe(val => {
       if (this.router.url.toString() == "/") {
         console.log(this.router.url.toString());
@@ -30,17 +33,20 @@ export class ProgressBarComponent {
       if (this.router.url.toString() == "/personalDetails") {
         console.log(this.router.url.toString());
         targetDiv1?.classList.add("p");
+        
 
       }
       else if (this.router.url.toString() == "/contactDetails") {
         targetDiv2?.classList.add("c");
-
+        l1?.classList.add("green");
       }
       else if (this.router.url.toString() == "/skill") {
         targetDiv3?.classList.add("s");
+        l2?.classList.add("green");
       }
       else if (this.router.url.toString() == "/workExperience") {
         targetDiv4?.classList.add("w");
+        l3?.classList.add("green");
       }
       else {
         targetDiv1?.classList.add("a");
